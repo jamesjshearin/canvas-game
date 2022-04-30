8219d60f90e796a422fccf2d244f734f85351051
James Shearin <31577828+jamesjshearin@users.noreply.github.com>
2022-04-30 14:55:09 -0700

    some whacky stuff

--- index.js
+++ index.js
@@ -233,17 +233,19 @@ function animate() {
 }
 
 addEventListener('click', (event) => {
-    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+    for(let i = 0; i < 8; i++){
+     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
     const velocity = {
-        x: Math.cos(angle) * 5,
-        y: Math.sin(angle) * 5
+        x: Math.cos(angle + 0.785 * i) * 5,
+        y: Math.sin(angle + 0.785 * i) * 5
     }
     projectiles.push(new Projectile(
         x,
         y,
         5,
         'white',
-        velocity))
+        velocity))   
+    }
 })
 
 startGameBtn.addEventListener('click', () => {
